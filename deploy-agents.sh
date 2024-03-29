2b37ac50684a44935837b0ebdc45596406138ae1
Marius Boeru <marius.boeru@bigstep.com>
2024-03-29 14:50:05 +0200

    fix resolv conf setup

--- deploy-agents.sh
+++ deploy-agents.sh
@@ -23,21 +23,9 @@ function debuglog ()
   echo -e "${mtypeis} ${!color}${msg}${nc}"
 }
 
-function parse_yaml {
-   local prefix=$2
-   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
-   sed -ne "s|^\($s\):|\1|" \
-        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
-        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
-   awk -F$fs '{
-      indent = length($1)/2;
-      vname[indent] = $2;
-      for (i in vname) {if (i > indent) {delete vname[i]}}
-      if (length($3) > 0) {
-         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
-      }
-   }'
+yamltojson ()
+{
+    python3 -c "import yaml;import json; yml = yaml.safe_load(open('$1')); x = json.dumps(yml); print(x)"
 }
 
 debuglog "whoami: $(whoami)" bold pink
